c43583bf2a8f025ee521c11c7970660f69705827
obhein381 <220661734+obhein381@users.noreply.github.com>
2026-03-17 11:04:25 +0900

    feat: implement basic shell loop with readline

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -10,8 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include "minishell.h"
+
+static char	*read_input(void)
 {
+	char	*input;
+
+	if (isatty(STDIN_FILENO))
+	{
+		input = readline("minishell$ ");
+		if (input == NULL)
+			exit(0);
+		if (*input)
+			add_history(input);
+	}
+	else
+		input = readline(NULL);
+	return (input);
+}
+
+int	main(void)
+{
+	char	*input;
+
+	while (1)
+	{
+		input = read_input();
+	}
 	return (0);
 }
+
 
