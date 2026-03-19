22ae00b90a1915a923f68af3374f8e60efa89ce4
obhein381 <220661734+obhein381@users.noreply.github.com>
2026-03-19 13:15:51 +0900

    chore: add debug function to inspect command fd values

--- srcs/debug/debug_commands.c
+++ srcs/debug/debug_commands.c
@@ -1,20 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser_word.c                                      :+:      :+:    :+:   */
+/*   debug_commands.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jisulim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/18 16:13:47 by jisulim           #+#    #+#             */
-/*   Updated: 2026/03/18 16:13:49 by jisulim          ###   ########.fr       */
+/*   Created: 2026/03/19 12:32:24 by jisulim           #+#    #+#             */
+/*   Updated: 2026/03/19 12:32:27 by jisulim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdio.h>
 
-t_command	*parser_word(t_command **commands, t_parser_state state)
+void	print_commands(t_command *commands)
 {
-	(void)commands;
-	(void)state;
-	return (*commands);
+	t_command	*head;
+	int			n;
+
+	n = 0;
+	head = commands;
+	while (head != NULL)
+	{
+		printf("n command fd_in == %d \n", head->fd_in);
+		printf("n command fd_out == %d \n", head->fd_out);
+		n++;
+		head = head->next;
+	}
+	return ;
 }
+
