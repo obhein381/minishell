93c869b4ab97c816a48ced9d80bf46425b06eb5e
obhein381 <220661734+obhein381@users.noreply.github.com>
2026-05-06 16:10:54 +0900

    fix(exit): handle overflow and normalize exit status

--- srcs/executor/builtin/execute_exit.c
+++ srcs/executor/builtin/execute_exit.c
@@ -11,13 +11,46 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "limits.h"
+
+long long	ft_atoll(const char *str, int *status)
+{
+	int			i;
+	int			check;
+	long long	result;
+	int			digit;
+
+	result = 0;
+	check = 1;
+	i = 0;
+	while ((9 <= str[i] && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			check = -1;
+		i++;
+	}
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		digit = str[i] - '0';
+		if (result > (LLONG_MAX - digit) / 10)
+		{
+			*status = 1;
+			return (0);
+		}
+		result = (result * 10) + (digit);
+		i++;
+	}
+	return (check * result);
+}
 
 static int	handling_error(t_shell *shell, char *argv, int status)
 {
 	if (status == 1)
 	{
 		write(2, "exit: too many arguments\n", 26);
-		return (status);
+		return (1);
 	}
 	else
 	{
@@ -25,7 +58,7 @@ static int	handling_error(t_shell *shell, char *argv, int status)
 		write(2, argv, ft_strlen(argv));
 		write(2, ": numeric argument required\n", 28);
 		shell->shut_down = 1;
-		return (status);
+		return (2);
 	}
 }
 
@@ -48,10 +81,13 @@ static int is_numeric(char *str)
 	return (1);
 }
 
+
 int	execute_exit(t_command *command, t_shell *shell)
 {
-	int	status;
+	long long	status;
+	int			sign;
 
+	sign = 0;
 	if (command->argv[1] == NULL)
 	{
 		shell->shut_down = 1;
@@ -61,8 +97,16 @@ int	execute_exit(t_command *command, t_shell *shell)
 		return (handling_error(shell, command->argv[1], 2));
 	if (command->argv[2] != NULL)
 		return (handling_error(shell, command->argv[1], 1));
+	if (ft_strlen(command->argv[1]) == 20
+	&& ft_strncmp(command->argv[1], "-9223372036854775808", 20) == 0)
+	{
+		shell->shut_down = 1;
+		return ((unsigned char)LLONG_MIN);
+	}
 	shell->shut_down = 1;
-	status = ft_atoi(command->argv[1]);
-	return (status);
+	status = ft_atoll(command->argv[1], &sign);
+	if (sign == 1)
+		return (handling_error(shell, command->argv[1], 2));
+	return ((unsigned char)status);
 }
 
